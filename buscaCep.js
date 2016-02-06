d8e844e3b3111900a8e0152e753aa8bb7ecd3da6
UpInside Treinamentos <cursos@upinside.com.br>
2016-02-06 19:27:54 -0200

    Create buscaCep.js

---
+++ buscaCep.js
@@ -0,0 +1,15 @@
+//BUSCA CEP
+$('.j_getCep').change(function () {
+    var cep = $(this).val().replace('-', '').replace('.', '');
+    if (cep.length == '8') {
+        $.get("https://viacep.com.br/ws/" + cep + "/json", function (data) {
+            if (!data.erro) {
+                $('.j_bairro').val(data.bairro);
+                $('.j_complemento').val(data.complemento);
+                $('.j_localidade').val(data.localidade);
+                $('.j_logradouro').val(data.logradouro);
+                $('.j_uf').val(data.uf);
+            }
+        }, 'json');
+    }
+});
